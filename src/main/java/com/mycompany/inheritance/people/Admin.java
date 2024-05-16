c7610b6ada35655702daf412387d097e74ee0fce
CamiiUribeee <acuribel@ufpso.edu.co>
2024-05-16 16:00:06 -0500

    inheritance

--- src/main/java/com/mycompany/inheritance/people/Admin.java
+++ src/main/java/com/mycompany/inheritance/people/Admin.java
@@ -2,8 +2,15 @@
 package com.mycompany.inheritance.people;
 
 
-public class Admin {
+public class Admin extends Person  {
     private String role; 
+
+    public Admin(String role, String name, String lastName, String id, int age) {
+        super(name, lastName, id, age);
+        this.role = role;
+    }
+    
+    
     
     public void doWork(){
         System.out.println("Hola estoy trabajando");
